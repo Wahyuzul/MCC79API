801e47f7ba86d952042a4f7bd0fc41da816df845
Wahyu Zul Fahmi <Wahyuzulfahmi14@gmail.com>
2023-07-02 20:57:01 +0700

    Update ForgotPasswordDto.cs

--- API/DTOs/Accounts/ForgotPasswordDto.cs
+++ API/DTOs/Accounts/ForgotPasswordDto.cs
@@ -7,9 +7,5 @@ namespace API.DTOs.Accounts
         [Required]
         [EmailAddress]
         public string Email { get; set; }
-
-        public int Otp { get; set; }
-
-        public DateTime ExpireTime { get; set; }
     }
 }
