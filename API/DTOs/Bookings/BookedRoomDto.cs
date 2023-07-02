c8aa84bb57de042df5c05a0251b355b39342104e
Wahyu Zul Fahmi <Wahyuzulfahmi14@gmail.com>
2023-07-02 20:56:48 +0700

    Create BookedRoomDto.cs

---
+++ API/DTOs/Bookings/BookedRoomDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Utilities;
+using System.Security.Cryptography.X509Certificates;
+
+namespace API.DTOs.Bookings
+{
+    public class BookedRoomDto
+    {
+        public Guid Guid { get; set; }
+        public string RoomName { get; set; }
+        public string BookedBy { get; set; }
+        public StatusLevel Status { get; set; }
+        public int Floor { get; set; }
+
+    }
+}
